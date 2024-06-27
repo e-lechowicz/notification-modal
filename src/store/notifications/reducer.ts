61bd168aa9e1b0949c34c98a4d2506697fec3850
e-lechowicz <prucvka@gmail.com>
2024-06-27 17:16:46 +0200

    Persist notifications across page reloads using localStorage

--- src/store/notifications/reducer.ts
+++ src/store/notifications/reducer.ts
@@ -4,85 +4,99 @@ import {
   NotificationActionTypes,
   NotificationsState,
 } from './types';
-import { NotificationType } from '../../types/models.ts';
+import { Notification, NotificationType } from '../../types/models.ts';
 
-const initialState: NotificationsState = {
+const initState: NotificationsState = {
   notifications: [
     {
       id: '1',
-      type: NotificationType.request,
-      message: 'New request for ',
-      date: 'dasd',
+      type: NotificationType.newFeature,
+      message: 'New feature!! ',
+      date: '2024-06-27',
       read: false,
     },
     {
       id: '2',
       type: NotificationType.onHold,
-      message: 'New request for <strong>Umpa Dumpa</strong>',
-      date: '',
+      message: '<strong>Bob Bob</strong> put the project on hold',
+      date: '2024-06-26',
       read: false,
     },
     {
       id: '3',
-      type: NotificationType.newFeature,
-      message: 'New request for <strong>Umpa Dumpa</strong>',
-      date: '',
+      type: NotificationType.onHold,
+      message: 'Sb put <strong>Some Project</strong> on hold',
+      date: '2024-06-24',
       read: false,
     },
-    // {
-    //   id: '4',
-    //   type: NotificationType.request,
-    //   message: 'New request for <strong>Umpa Dumpa</strong>',
-    //   date: '',
-    //   read: false,
-    // },
-    // {
-    //   id: '5',
-    //   type: NotificationType.request,
-    //   message: 'New request for <strong>Umpa Dumpa</strong>',
-    //   date: '',
-    //   read: false,
-    // },
-    // {
-    //   id: '6',
-    //   type: NotificationType.request,
-    //   message: 'New request for <strong>Umpa Dumpa</strong>',
-    //   date: '',
-    //   read: false,
-    // },
-    // {
-    //   id: '7',
-    //   type: NotificationType.request,
-    //   message: 'New request for <strong>Umpa Dumpa</strong>',
-    //   date: '',
-    //   read: false,
-    // },
-    // {
-    //   id: '8',
-    //   type: NotificationType.request,
-    //   message: 'New request for <strong>Umpa Dumpa</strong>',
-    //   date: '',
-    //   read: false,
-    // },
+    {
+      id: '4',
+      type: NotificationType.request,
+      message:
+        '<strong>Anna Kowal</strong> has requested sth for<strong>Project Y</strong>',
+      date: '2024-05-27',
+      read: false,
+    },
+    {
+      id: '5',
+      type: NotificationType.onHold,
+      message:
+        '<strong>Anna Kowal</strong> from <strong>Company X</strong> has put the project <strong>Project Z</strong> on hold',
+      date: '2024-05-27',
+      read: false,
+    },
+    {
+      id: '6',
+      type: NotificationType.request,
+      message:
+        '<strong>Jan Nowak</strong> from <strong>MindPal</strong> has requested a Mindocument for their project <strong>Minddev</strong>',
+      date: '2024-05-21',
+      read: false,
+    },
+    {
+      id: '7',
+      type: NotificationType.newFeature,
+      message: '<strong>New Project X feature: Y</strong>',
+      date: '2024-05-16',
+      read: false,
+    },
+    {
+      id: '8',
+      type: NotificationType.newFeature,
+      message: '<strong>New MindTool feature: AI Assistant</strong>',
+      date: '2024-05-10',
+      read: false,
+    },
   ],
 };
 
+const loadInitialState = (): NotificationsState => {
+    const storedNotifications = localStorage.getItem('notifications');
+    if (storedNotifications) {
+        return { notifications: JSON.parse(storedNotifications) as Notification[] };
+    }
+    return initState;
+};
+
+const initialState: NotificationsState = loadInitialState();
+
 const notificationsReducer = (state = initialState, action: NotificationActionTypes): NotificationsState => {
+    const allReadNotifications = state.notifications.map((n) => ({ ...n, read: true }));
+    const updatedNotifications = ('payload' in action) && state.notifications.map((n) =>
+        n.id === action.payload.id ? { ...n, read: true } : n
+    );
     switch (action.type) {
         case MARK_AS_READ:
-            if ('payload' in action && action.payload) {
-                return {
-                    ...state,
-                    notifications: state.notifications.map((n) =>
-                        n.id === action.payload.id ? { ...n, read: true } : n
-                    ),
-                };
-            }
-            return state;
-        case MARK_ALL_AS_READ:
+            localStorage.setItem('notifications', JSON.stringify(updatedNotifications));
             return {
                 ...state,
-                notifications: state.notifications.map((n) => ({ ...n, read: true })),
+                notifications: updatedNotifications,
+            };
+        case MARK_ALL_AS_READ:
+            localStorage.setItem('notifications', JSON.stringify(allReadNotifications));
+            return {
+                ...state,
+                notifications: allReadNotifications,
             };
         default:
             return state;
