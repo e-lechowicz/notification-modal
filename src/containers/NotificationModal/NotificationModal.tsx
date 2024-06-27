324a5af8786a5d4550c8d5916c4114b364d5d90f
e-lechowicz <prucvka@gmail.com>
2024-06-27 16:52:54 +0200

    updated modal styles

--- src/containers/NotificationModal/NotificationModal.tsx
+++ src/containers/NotificationModal/NotificationModal.tsx
@@ -6,14 +6,18 @@ import React, {
   useState,
 } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import cn from 'classnames';
+
 import {
   markAllAsRead as markAllAsReadAction,
   markAsRead as markAsReadAction,
 } from '../../store/notifications/actions.ts';
 import { NotificationActionTypes } from '../../store/notifications/types.ts';
+
 import Button from '../../atoms/Button/Button.tsx';
 import NotificationList from '../../components/NotificationList/NotificationList.tsx';
 import { RootState } from '../../store/store.ts';
+import NotificationsCount from '../../atoms/NotificationCount/NotificationsCount.tsx';
 
 type NotificationsTab = 'all' | 'unread';
 
@@ -44,8 +48,17 @@ const NotificationModal: React.FC<Props> = ({ visible, onClose }) => {
   
   useEffect(() => {
     // changing the state to rerender the component
-    setKey(k => k+1)
+    setKey(k => k + 1)
   },[notifications])
+
+  const getButtonStyles = useCallback((tab: NotificationsTab) => {
+    return cn('w-auto pt-0 pb-0 font-semibold text-s rounded-3xl', {
+      'border-2 border-gray-600 text-gray-600': activeTab === tab,
+      'bg-gray-200 text-gray-600 hover:bg-gray-100': activeTab !== tab,
+    });
+  }, [activeTab]);
+  
+  const notificationsCount = useMemo(()=> unreadNotifications.length, [unreadNotifications.length])
   
   return visible ? (
     <>
@@ -53,14 +66,34 @@ const NotificationModal: React.FC<Props> = ({ visible, onClose }) => {
         className="fixed top-0 left-0 w-full h-full bg-black opacity-0 flex items-center justify-center"
         onClick={onClose}
       ></div>
-      <div key={key} className="fixed top-16 right-4 bg-white p-6 rounded-lg shadow-lg min-w-[650px]">
-        <div>
-          <div>
-            <Button onClick={setAllTab}>All</Button>
-            <Button onClick={setUnreadTab}>Unread</Button>
-            <button onClick={handleMarkAllAsRead}>
-              Mark all as read
-            </button>
+      <div
+        key={key}
+        className="fixed top-16 right-4 bg-white p-6 rounded-lg shadow-lg min-w-[650px]"
+      >
+        <>
+          <div className="flex mb-6">
+            <div className="ml-3 text-2xl font-semibold mr-1">
+              {'Notifications'}
+            </div>
+            {notificationsCount>0 &&
+                <NotificationsCount className="p-1 h-5 w-5 text-xs" count={notificationsCount} />}
+          </div>
+          <div className="flex mb-5 gap-5">
+            <Button className={getButtonStyles('all')} onClick={setAllTab}>
+              All Notifications
+            </Button>
+            <Button
+              className={getButtonStyles('unread')}
+              onClick={setUnreadTab}
+            >
+              Unread Notifications
+            </Button>
+            <Button className='flex' onClick={handleMarkAllAsRead}>
+              <div className='bg-check-icon bg-no-repeat bg-contain p-4'/>
+              <div className='text-sm font-bold text-blue-600 ml-1 mt-1 hover:text-blue-500'>
+                {'Mark all as read'}
+              </div>
+            </Button>
           </div>
           {activeTab === 'unread' ? (
             <NotificationList
@@ -73,7 +106,7 @@ const NotificationModal: React.FC<Props> = ({ visible, onClose }) => {
               markAsRead={handleMarkAsRead}
             />
           )}
-        </div>
+        </>
       </div>
     </>
   ) : null;
